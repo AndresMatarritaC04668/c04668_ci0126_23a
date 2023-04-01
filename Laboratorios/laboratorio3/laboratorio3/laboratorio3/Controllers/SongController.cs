4a1f980b18ca75424d6a7b5b85edb84f2f11b345
AndresMatarritaC04668 <andresmm1919@hotmail.com>
2023-04-01 13:33:37 -0600

    Creacion de proyecto MVC con nueva funcionalidad "Song"

---
+++ Laboratorios/laboratorio3/laboratorio3/laboratorio3/Controllers/SongController.cs
@@ -0,0 +1,25 @@
+﻿using laboratorio3.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace laboratorio3.Controllers
+{
+    public class SongController : Controller
+    {
+        public IActionResult Index()
+        {
+            var song = GetSong();
+            ViewBag.MainTitle = "Mi cancion preferida";
+            return View(song);
+        }
+        private SongModel GetSong()
+        {
+            SongModel song = new SongModel();
+            song.Name = "Perfect";
+            song.Singer = "Ed Sheeran";
+            song.Genre = "Pop/Balada romántica";
+            song.ViewsYoutube = 340800509;
+            song.ReleaseDate = new DateTime(2017, 9, 8);
+
+            return song;
+        }
+    }
+}
---
+++ Laboratorios/laboratorio3/laboratorio3/laboratorio3/Models/SongModel.cs
@@ -0,0 +1,16 @@
+﻿namespace laboratorio3.Models
+{
+    public class SongModel
+    {
+        public string Singer { get; set; }
+
+        public string Name { get; set; }
+
+        public string Genre { get; set; }
+
+        public int ViewsYoutube { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
