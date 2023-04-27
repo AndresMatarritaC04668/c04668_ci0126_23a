ee08d4ccbf987cecb93f41a028cc2f9a6ebb133e
AndresMatarritaC04668 <andresmm1919@hotmail.com>
2023-04-27 16:22:52 -0600

    Entregables del lab 5 reporte y proyecto visual

---
+++ Laboratorios/laboratorio5/Laboratorio5/Controllers/PeliculasController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Laboratorio5.Handlers;
+
+namespace Laboratorio5.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        public IActionResult Index()
+        {
+            PeliculasHandler peliculasHandler = new PeliculasHandler();
+            var peliculas = peliculasHandler.ObtenerPeliculas();
+            ViewBag.MainTitle = "Lista de Peliculas";
+            return View(peliculas);
+        }
+    }
+}
---
+++ Laboratorios/laboratorio5/Laboratorio5/Models/PeliculaModelo.cs
@@ -0,0 +1,11 @@
+﻿namespace Laboratorio5.Models
+{
+    public class PeliculaModelo
+    {
+        public int ID { get; set; }
+
+        public string Nombre { get; set; }
+
+        public int Año { get; set; }
+    }
+}
