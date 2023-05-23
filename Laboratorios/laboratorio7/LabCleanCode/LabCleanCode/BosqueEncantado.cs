84a3ac0aafb207067352d0f6167b32d8a10db36e
AndresMatarritaC04668 <andresmm1919@hotmail.com>
2023-05-23 14:06:49 -0600

    Laboratorio 7 completo con reporte y codigo

---
+++ Laboratorios/laboratorio7/LabCleanCode/LabCleanCode/BosqueEncantado.cs
@@ -0,0 +1,110 @@
+﻿namespace LabCleanCode
+{
+    public class BosqueEncantado
+    {
+        private int[,] matrizBosque;
+        private int cantidadFilas;
+        private int cantidadColumnas;
+        //LAGO ENCANTADO: 1
+        //ARBOL: 2
+        //VACIO: 3
+
+        public BosqueEncantado(int pfilas, int pcolumnas)
+        {
+            cantidadFilas = pfilas;
+            cantidadColumnas = pcolumnas;
+            matrizBosque = new int[cantidadFilas, cantidadColumnas];
+            double doubleAleatorio;
+            int int_decision;
+            for (int f = 0; f < cantidadFilas; ++f)
+            {
+                for (int c = 0; c < cantidadColumnas; ++c)
+                {
+                    doubleAleatorio = (Math.random() * 3) + 1;
+                    int_decision = (int)numeroAleatorio;
+                    if (int_desicion == 1)
+                    {
+                        matrizBosque[f,c] = 1;
+                    }
+                    if (int_decision == 2)
+                    {
+                        matrizBosque[f,c] = 2;
+                    }
+
+                    if (int_decision == 3)
+                    {
+                        matrizBosque[f,c] = 3;
+                    }
+                    if (int_decision != 3 && int_decision != 2 && int_decision != 1)
+                    {
+                        doubleAleatorio = (Math.random() * 3) + 1;
+                    }
+                }
+            }
+        }
+
+
+        public int ContarCeldasAdyacentes(int pf, int pc, int targetValue)
+        {
+            int pIntCont = 0;
+
+            for (int estaFila = pf - 1; estaFila <= pf + 1; ++estaFila)
+            {
+                for (int estaColumna = pc - 1; estaColumna <= pc + 1; ++estaColumna)
+                {
+                    if (!(estaFila == pf && estaColumna == pc))
+                    {
+                        if (0 <= estaFila && estaFila < cantidadFilas && 0 <= estaColumna && estaColumna < cantidadColumnas)
+                        {
+                            if (matrizBosque[estaFila, estaColumna] == targetValue)
+                                ++pIntCont;
+                        }
+                    }
+                }
+            }
+
+            return pIntCont;
+        }
+
+
+        public void cambiarnumerosdelbosque()
+        {
+            for (int f = 0; f < cantidadFilas; ++f)
+            {
+                for (int c = 0; c < cantidadColumnas; ++c)
+                {
+                    int contadorArboles = ContarCeldasAdyacentes(f,c,1);
+                    int contadorLagos = ContarCeldasAdyacentes(f,c,2);
+                    if (matrizBosque[f,c] == 2)
+                    {
+                        if (contadorLagos >= 4)
+                        {
+                            matrizBosque[f,c] = 1;
+                        }
+                        else
+                        {
+                            if (contadorArboles > 4)
+                            {
+                                matrizBosque[f,c] = 2;
+                            }
+                        }
+                    }
+                    if (matrizBosque[f,c] == 1)
+                    {
+                        if (contadorLagos < 3)
+                            matrizBosque[f,c] = 3;
+                    }
+
+                    if (matrizBosque[f,c] == 3)
+                    {
+                        if (contadorArboles >= 3)
+                            matrizBosque[f,c] = 2;
+                    }
+                }
+            }
+			Pantalla p = new Pantalla(matrizBosque);
+			string s=p.crear();
+			//imp pantalla
+        }
+    }
+}
---
+++ Laboratorios/laboratorio7/LabCleanCode/LabCleanCode/Hangman.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+
+namespace LabCleanCode
+{
+    class Hangman
+    {
+        public static void PlayHangman()
+        {
+
+            Console.WriteLine("Welcome to Hangman!!!!!!!!!!");
+
+            StreamReader file = new StreamReader("usa.txt");
+            Random randGen = new Random();
+            int randomIndex = randGen.Next(0, 61333);
+
+            //int randomIndex = randGen.Next(0, 10);
+            string mysteryWord = new string("");
+            for (int i = 0; i < idx; i++) {
+                mysteryWord = file.ReadLine();
+            }
+            file.Close();
+
+            char[] guess = new char[mysteryWord.Length];
+            Console.Write("Please enter your guess: ");
+
+            for (int p = 0; p < mysteryWord.Length; p++)
+                guess[p] = '*';
+
+
+            while (true)
+            {
+                char playerGuess = 'A';
+                try
+                {
+                    playerGuess = char.Parse(Console.ReadLine());
+                }
+                catch (FormatException e)
+                {
+                    playerGuess = ' ';
+                }
+                for (int j = 0; j < mysteryWord.Length; j++)
+                {
+                    if (playerGuess == mysteryWord[j])
+                        guess[j] = playerGuess;
+                }
+                string a = new string(guess);
+                if (!gano(a, mysteryWord))
+                {
+                    //Sigue jugando
+                }
+                else
+                {
+                    break;
+                }
+                Console.WriteLine(guess);
+            }
+            Console.ReadLine();
+        }
+
+
+
+
+
+        // Validación del juego
+
+        public static bool gano(string word1, string word2)
+        {
+            if (word1.Equals(word2))
+            {
+                Console.Write("Congratulations the word was: " + word2);
+                return true;
+            }
+            return false;
+        }
+
+        public void readdata()
+        {
+            FileStream fs = new FileStream("usa.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            string str = sr.ReadLine();
+            while (str != null)
+            {
+                Console.WriteLine("{0}", str);
+                str = sr.ReadLine();
+            }
+            //Close the Writer and File
+            sr.Close();
+            fs.Close();
+        }
+
+    }
+}
