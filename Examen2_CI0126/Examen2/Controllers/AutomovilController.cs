7e73f61a5bd8e55d46f443de2671834d20f2fa64
AndresMatarritaC04668 <andresmm1919@hotmail.com>
2023-07-08 00:12:04 -0600

    Se crea un controlador automovilController  y dentro de esto se implementa el metodo de AgregarAutomovil para manejar dicha vista con HTTP POST y GET

---
+++ Examen2_CI0126/Examen2/Controllers/AutomovilController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Examen2.Models;
+namespace Examen2.Controllers
+{
+    public class AutomovilController : Controller
+    {
+        [HttpGet]
+        public IActionResult AgregarAutomovil()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AgregarAutomovil(AutomovilModel automovil)
+        {
+            return View();
+        }
+    }
+}
