763d2b773218b2aaa3a7058f7bc30a8e8df77541
AndresMatarritaC04668 <andresmm1919@hotmail.com>
2023-07-08 00:59:34 -0600

    Se conecta la base de datos al proyecto por medio de un connection string y se crea un handler abstracto que crea la conexion con la base para que no se repita lo mismo en todos los demas handlers

---
+++ Examen2_CI0126/Examen2/Handlers/HandlerAbstracto.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Examen2.Handlers
+{
+    public abstract class HandlerAbstracto
+    {
+
+        protected SqlConnection conexion;
+        protected string rutaConexion;
+        public HandlerAbstracto()
+        {
+            var builder = WebApplication.CreateBuilder();
+            rutaConexion =
+            builder.Configuration.GetConnectionString("ContextoJunquillal");
+            conexion = new SqlConnection(rutaConexion);
+        }
+
+        //método para llenar una tabla a partir de la información obtenida de una consulta a la base de datos
+        protected DataTable CrearTablaConsulta(string consulta)
+        {
+
+            SqlCommand comandoParaConsulta = new SqlCommand(consulta,
+            conexion);
+            SqlDataAdapter adaptadorParaTabla = new
+            SqlDataAdapter(comandoParaConsulta);
+            DataTable consultaFormatoTabla = new DataTable();
+            conexion.Open();
+            adaptadorParaTabla.Fill(consultaFormatoTabla);
+            conexion.Close();
+            return consultaFormatoTabla;
+        }
+
+        public void reuniciarConexion()
+        {
+            var builder = WebApplication.CreateBuilder();
+            rutaConexion =
+            builder.Configuration.GetConnectionString("ContextoJunquillal");
+            conexion = new SqlConnection(rutaConexion);
+        }
+    }
+}
