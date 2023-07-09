2ebf17e3199e598a4be2af86958735b9cd7a13a8
AndresMatarritaC04668 <andresmm1919@hotmail.com>
2023-07-09 13:39:14 -0600

    Se crean dos pruebas unitarias para testear el metodo de agregar automoviles en el handler de automovil y probar distintos casos uno correcto y otro incorrecto

---
+++ Examen2_CI0126/unit_tests/Examen2_Unit_Test/Handlers/AgregarAutomovil_Test.cs
@@ -0,0 +1,66 @@
+﻿using Examen2.Handlers;
+using Examen2.Models;
+
+namespace Examen2_Unit_Test.Handlers
+{
+    [TestClass]
+    public class AgregarAutomovil_Test
+    {
+        private AutomovilHandler automovilHandler;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            automovilHandler = new AutomovilHandler();
+        }
+
+        [TestMethod]
+        // Justificación de la selección de los valores de entrada:
+        // En esta prueba, utilizamos valores válidos para todos los parámetros del automóvil.
+        // Esperamos que la consulta se ejecute correctamente y se agregue el automóvil al concesionario.
+        // Resultado esperado en palabras: La consulta se ejecuta correctamente y se agrega el automóvil al concesionario.
+        public void AgregarAutomovil_ValidParameters_ReturnsTrue()
+        {
+            // Arrange
+            AutomovilModel automovil = new AutomovilModel
+            {
+                Marca = "Toyota",
+                Modelo = "RAV4",
+                Color = "Azul",
+                NumeroPuertas = 2,
+                DobleTraccion = false
+            };
+
+            // Act
+            bool resultado = automovilHandler.AgregarAutomovil(automovil);
+
+            // Assert
+            Assert.IsTrue(resultado);
+        }
+
+        [TestMethod]
+        // Justificación de la selección de los valores de entrada:
+        // En esta prueba, pasamos un valor nulo para la marca.
+        // Esperamos que se lance una excepción debido a que la marca no puede ser nula.
+        // Resultado esperado en palabras: Se lanza una excepción debido a un parámetro obligatorio faltante.
+        [ExpectedException(typeof(Exception), "Error al ejecutar la consulta: La marca no puede ser nula.")]
+        public void AgregarAutomovil_NullMarca_ThrowsException()
+        {
+            // Arrange
+            AutomovilModel automovil = new AutomovilModel
+            {
+                Marca = null,
+                Modelo = "Corolla",
+                Color = "Rojo",
+                NumeroPuertas = 4,
+                DobleTraccion = false
+            };
+
+            // Act
+            automovilHandler.AgregarAutomovil(automovil);
+
+            // Assert
+            // ExpectedException attribute handles the assertion
+        }
+    }
+}
